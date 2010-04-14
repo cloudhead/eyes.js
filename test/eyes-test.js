2d061d058e8e671152798a09642ce4bb162e7a59
cloudhead <self@cloudhead.net>
2010-04-14 18:25:09 -0400

    test/ Makefile

---
+++ test/eyes-test.js
@@ -0,0 +1,29 @@
+var sys = require('sys');
+var eyes = require('../lib/eyes');
+
+eyes.inspect({
+    number: 42,
+    string: "John Galt",
+    regexp: /[a-z]+/,
+    array: [99, 168, 'x', {}],
+    func: function () {},
+    bool: false,
+    nil: null,
+    undef: undefined,
+    object: {attr: []}
+}, "native types");
+
+eyes.inspect({
+    number: new(Number)(42),
+    string: new(String)("John Galt"),
+    regexp: new(RegExp)(/[a-z]+/),
+    array: new(Array)(99, 168, 'x', {}),
+    bool: new(Boolean)(false),
+    object: new(Object)({attr: []}),
+    date: new(Date)
+}, "wrapped types");
+
+var inspect = eyes.inspector({ writer: false });
+
+sys.puts(inspect('something', "something"));
+sys.puts(inspect("something else"));
